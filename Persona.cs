d0ce57b2b59c1845d810d23a2d2dd7dbf941ef06
JuanfeRomero <juanferomerocode@gmail.com>
2021-10-05 15:05:58 -0300

    Agregar archivos de proyecto.

---
+++ Persona.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ComprobacionDTO
+{
+    class Persona
+    {
+        public string Nombre { get; set; }
+        public string Apellido { get; set; }
+        public int Edad { get; set; }
+        public DateTime FechaNacimiento { get; set; }
+
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Globalization;
+
+namespace ComprobacionDTO
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Persona persona = new Persona();
+            persona.Nombre ="Juan Federico";
+            persona.Apellido = "Romero";
+            persona.Edad = 26;
+            persona.FechaNacimiento = Convert.ToDateTime("09/02/1995", new CultureInfo("es-ES"));
+
+            Console.WriteLine("Nombre: " + persona.Nombre);
+            Console.WriteLine("Apellido: " + persona.Apellido);
+            Console.WriteLine("Edad: " + persona.Edad);
+            Console.WriteLine("Fecha de Nacimiento: " + persona.FechaNacimiento.ToString("dd-MM-yyyy"));
+
+            Console.WriteLine("Presione cualquier boton para terminar...");
+            Console.ReadKey();
+        }
+    }
+}
